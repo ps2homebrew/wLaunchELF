d813339f98e76b99f94e65b13cd54c2fe178f36b
SP193 <pirorin187@gmail.com>
2019-01-11 16:21:51 +0800

    Updated to use USBMASS_DEVCTL_STOP_ALL instead, to shut down all USB devices without knowing the actual number connected.

--- main.c
+++ main.c
@@ -1167,8 +1167,6 @@ void loadHdlInfoModule(void)
 //---------------------------------------------------------------------------
 static void closeAllAndPoweroff(void)
 {
-	int i;
-
 	if (ps2dev9_loaded) {
 		/* Close all files */
 		fileXioDevctl("pfs:", PDIOC_CLOSEALL, NULL, 0, NULL, 0);
@@ -1178,11 +1176,7 @@ static void closeAllAndPoweroff(void)
 	}
 
 	// As required by some (typically 2.5") HDDs, issue the SCSI STOP UNIT command to avoid causing an emergency park.
-	for (i = 0; i < USB_mass_max_drives; i++) {
-		char device[7];
-		sprintf(device, "mass%d:", i);
-		fileXioDevctl(device, USBMASS_DEVCTL_STOP_UNIT, NULL, 0, NULL, 0);
-	}
+	fileXioDevctl("mass:", USBMASS_DEVCTL_STOP_ALL, NULL, 0, NULL, 0);
 
 	/* Power-off the PlayStation 2 console. */
 	poweroffShutdown();
